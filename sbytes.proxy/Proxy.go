6d5f5ee1af21a2dc3fd347d87e21abeeed6a587b
antonin <antonin.artaud.aa@gmail.com>
2022-05-08 21:20:13 +0200

    rename credential struct

--- sbytes.proxy/Proxy.go
+++ sbytes.proxy/Proxy.go
@@ -5,13 +5,15 @@ import (
 	"log"
 )
 
-type credential struct {
+type ScanQrCodeRequest struct {
+	FireBaseId string `json:"fireBaseId" binding:"required"`
 	Url        string `json:"url" binding:"required"`
 	Credential struct {
 		Id       string `json:"id" binding:"required"`
 		Password string `json:"password" binding:"required"`
 	} `json:"credential" binding:"required"`
 }
+
 type iProxy interface {
 	handleAuthRequest(ctx *gin.RouterGroup)
 }
@@ -26,14 +28,14 @@ func NewProxy() *proxy {
 
 func (receiver *proxy) handleAuthRequest(context *gin.RouterGroup) {
 	context.POST("/login", func(ctx *gin.Context) {
-		var data credential
-		err := ctx.BindJSON(&data)
+		var requestData ScanQrCodeRequest
+		err := ctx.BindJSON(&requestData)
 
 		if err != nil {
 			log.Printf("somethings went wrong with post data...")
 		}
 
 		log.Printf("url is '%s', id is '%s', and password is '%s'",
-			data.Url, data.Credential.Id, data.Credential.Password)
+			requestData.Url, requestData.Credential.Id, requestData.Credential.Password)
 	})
 }
